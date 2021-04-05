b878463af504fa5db58d512484243435ef9b8aab
Paranoid21 <waso1837@gmail.com>
2021-04-05 22:40:51 +0300

    button clear

--- code.py
+++ code.py
@@ -18,12 +18,14 @@ def eng_to_rus(city):
     result = translator.translate(city, src='en', dest='ru')
     p = morph.parse(result.text)[0]
     city = p.inflect({'loct'}).word
+    if len(city) > 1:
+        city = city.split()[-1]
     return city[0].upper() + city[1:]
 
 
 def get_weather():
     city = cityField.get()
-    api_key = 'Ваш ключ'  # нужно получить на OpenWeathepMap ссылка: https://openweathermap.org/
+    api_key = '28eded49f31842a06cc280df5ab95800'  # нужно получить на OpenWeathepMap ссылка: https://openweathermap.org/
     url = 'http://api.openweathermap.org/data/2.5/weather?'
     params = {'APPID': api_key, 'q': city}
     result = requests.get(url, params=params)
@@ -33,6 +35,10 @@ def get_weather():
                    f'Скорость ветера: {weather["wind"]["speed"]}м/с\n' \
                    f'Облачность: {weather["clouds"]["all"]}%\n' \
                    f'Видимость: {weather["visibility"]}м'
+
+
+def clear():
+    cityField.delete(0, 'end')
 
 
 root['bg'] = '#fafafa'
@@ -55,5 +61,10 @@ btn.pack()
 # Инфомарция о погоде
 info = Label(frame_bottom, text='Информация о погоде', bg='#ffb700', font=('Helvetica', 11))
 info.pack()
+
+# Добавить кнопку clear
+cls_btn = Button(frame_top, text='X', command=clear)
+cls_btn.pack()
+cls_btn.place(height=21, width=21, relx=0.82, rely=0)
 
 root.mainloop()
